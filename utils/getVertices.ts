623dd123256c61c65f3d2ae1578a5b84b38c1900
Xavier Alfaro <nxb.developer@gmail.com>
2023-01-18 19:52:20 -0500

    add types to getVertices function

--- utils/getVertices.ts
+++ utils/getVertices.ts
@@ -1,10 +1,14 @@
-export const getVertices = (boundings: any) => {
-  return boundings.map((bounding: any) => {
-    const {
-      boundingPoly: { vertices }
-    } = bounding
+import { google } from '@google-cloud/vision/build/protos/protos'
+
+export const getVertices = (boundings: google.cloud.vision.v1.IEntityAnnotation[]) => {
+  return boundings.map((bounding: google.cloud.vision.v1.IEntityAnnotation) => {
+    const { boundingPoly } = bounding
+    if (!boundingPoly)
+      return {
+        vertices: []
+      }
     return {
-      vertices
+      vertices: boundingPoly.vertices
     }
   })
 }
