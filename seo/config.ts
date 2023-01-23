1fa9ab33410f9b043d17ea0a17246d44c5815bc4
Xavier Alfaro <nxb.developer@gmail.com>
2023-01-23 14:19:59 -0500

    add favicon

--- seo/config.ts
+++ seo/config.ts
@@ -4,6 +4,12 @@ const config: DefaultSeoProps = {
   title: 'TextScan',
   description: 'textscan for getting text content from your images',
   defaultTitle: 'TextScan',
+  additionalLinkTags: [
+    {
+      rel: 'icon',
+      href: '/icons/favicon.ico'
+    }
+  ],
   openGraph: {
     type: 'website',
     url: 'https://textscan.vercel.app/',
